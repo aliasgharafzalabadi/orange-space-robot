fd818d2993185b29ffba876d12a7e7f4e3525af6
aliasghar_afzalabadi <74911932+aliasgharafzalabadi@users.noreply.github.com>
2024-01-04 13:39:15 +0000

    Pending changes exported from your codespace

---
+++ db.js
@@ -0,0 +1,24 @@
+const { MongoClient } = require('mongodb');
+
+// Replace 'your-mongodb-uri' with your actual MongoDB connection string
+const uri = 'mongodb+srv://ali:1381@cluster0.rsax1ad.mongodb.net/';
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function connectToMongoDB() {
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+    return client.db('blog');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    throw error;
+  }
+}
+
+function closeMongoDBConnection() {
+  client.close();
+  console.log('MongoDB connection closed');
+}
+
+module.exports = { connectToMongoDB, closeMongoDBConnection };
